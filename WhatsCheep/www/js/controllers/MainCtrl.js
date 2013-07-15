0a36a3dee62de55aa0d2f075abae25465f615dc2
Adiel Sharabi <adiel@hilash.net>
2013-07-15 17:39:30 +0300

    progress

--- WhatsCheep/www/js/controllers/MainCtrl.js
+++ WhatsCheep/www/js/controllers/MainCtrl.js
@@ -2,8 +2,16 @@ var WhatsCheep;
 (function (WhatsCheep) {
     var MainCtrl = (function () {
         function MainCtrl($scope) {
+            var _this = this;
             this.$scope = $scope;
-            $scope.Text = "Test Text";
+            $scope.message = "Test Text";
+            $scope.calc = function () {
+                return _this.calc();
+            };
+            $scope.amount1 = 0;
+            $scope.price1 = 0;
+            $scope.amount2 = 0;
+            $scope.price2 = 0;
             console.log("Inside ctor");
         }
         MainCtrl.prototype.injection = function () {
@@ -11,7 +19,10 @@ var WhatsCheep;
         };
 
         MainCtrl.prototype.calc = function () {
-            this.$scope.message = "Test";
+            var price1calc = this.$scope.price1 / this.$scope.amount1;
+            var price2calc = this.$scope.price2 / this.$scope.amount2;
+
+            this.$scope.message = "price 1:" + price1calc + " price 2: " + price2calc;
         };
         return MainCtrl;
     })();
--- WhatsCheep/www/js/controllers/MainCtrl.ts
+++ WhatsCheep/www/js/controllers/MainCtrl.ts
@@ -6,13 +6,23 @@ module WhatsCheep{
         }
         constructor(private $scope: any)
         {
-            $scope.Text = "Test Text";
+            $scope.message = "Test Text";
+            $scope.calc = () => this.calc();
+            $scope.amount1 = 0; 
+            $scope.price1 = 0; 
+            $scope.amount2 = 0; 
+            $scope.price2 = 0;
             console.log("Inside ctor");
         }
         
         public calc():void
         {
-            this.$scope.message = "Test";
+            var price1calc = this.$scope.price1 / this.$scope.amount1; 
+            var price2calc = this.$scope.price2 / this.$scope.amount2; 
+
+            this.$scope.message = "price 1:" + price1calc +
+            " price 2: " + price2calc;
+
         }
     }
 }
